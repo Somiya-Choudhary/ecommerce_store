4d68bb858e8221487da3ac9f71391fb8c833a691
Somiya <isomiyasj2@gmail.com>
2026-01-19 17:39:24 +0530

    Setup server with health check

---
+++ server/src/controllers/health.controller.js
@@ -0,0 +1,3 @@
+export const healthCheck = (req, res) => {
+  res.json({ status: "OKAY!! bye bye" });
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import healthRoutes from "./routes/health.routes.js";
 
 const app = express();
 
+//middlewares
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/health", (req, res) => {
-  res.json({ status: "OK" });
-});
+// routes
+app.use("/api", healthRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
